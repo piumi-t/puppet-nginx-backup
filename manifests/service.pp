7a36e4f23c1bc581bc3df836d580e83f2232e5f0
root <root@shashiudawa4.mylabserver.com>
2017-10-30 02:53:39 +0000

    service enable

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,7 +1,7 @@
 class nginx::service(
   String $ensure       = $::nginx::service_ensure,
   String $service_name = $::nginx::service_name,
-  Boolean $enable      = $::nginx::service_enable,
+  Boolean $service_enable      = $::nginx::service_enable,
   String $pattern      = $::nginx::service_pattern,
   Boolean $hasstatus   = $::nginx::service_hasstatus,
   Boolean $hasrestart  = $::nginx::service_hasrestart,
@@ -9,7 +9,7 @@ class nginx::service(
  service { 'nginx_service':
    ensure     => $ensure,
    name       => $service_name,
-   enable     => $enable,
+   enable     => $service_enable,
    pattern    => $pattern,
    hasstatus  => $hasstatus,
    hasrestart => $hasrestart,
