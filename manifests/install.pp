b60debb2a1a605b0f082c0b316b2e6c16f20742c
root <root@shashiudawa4.mylabserver.com>
2017-10-30 03:02:05 +0000

    fixed install

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,5 @@
 class nginx::install(
-  Srting $ensure       = $::nginx::ensure,
+  String $ensure       = $::nginx::ensure,
   String $package_name = $::nginx::package_name,
 ) {
   package { 'install_nginx':
